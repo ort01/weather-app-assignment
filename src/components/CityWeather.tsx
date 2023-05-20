9a74ef54b34e2082b002d42210292d1c88db915e
Miroslav <miroslav@vali.sk>
2023-05-20 16:35:37 +0200

    Use only required data as CityWeather component params

--- src/components/CityWeather.tsx
+++ src/components/CityWeather.tsx
@@ -1,10 +1,10 @@
-import React, { useEffect } from 'react';
+import React, { FunctionComponent, useEffect } from 'react';
+import { toDate } from './tools';
+import { CityWeatherProps } from '../types/CityWeather';
 
-export const CityWeather = () => {
+export const CityWeather: FunctionComponent<CityWeatherProps> = ({ name, country, description, icon, windSpeed, temp }) => (
+  <div>
+    CityWeather
+  </div>
+)
 
-  return (
-    <div>
-      CityWeather
-    </div>
-  )
-}
---
+++ src/types/CityWeather.ts
@@ -0,0 +1,8 @@
+export type CityWeatherProps = {
+  name: string;
+  country: string;
+  description: string;
+  icon: string;
+  windSpeed: number;
+  temp: number;
+};
